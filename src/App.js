5a69dc3756ffe9f7f6636d7f29b0bf3d604251a7
Boadu George Junior <pielly16@gmail.com>
2022-03-30 19:30:32 +0000

    elof made

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ import image from "./Images/Ellof_background1.jpg"
 const App = () =>{
   const year = document.getElementById("year")
   const d = new Date();
-   year.innerText = d.getFullYear();
+  //  year.innerText = d.getFullYear();
 
   return (
     <div className="container" style={{ backgroundImage:`url(${image})`}} >
       <Nav/>
      <Outlet/>
-     <p id='copyright'>Copyright &copy;<span id='year'></span><br></br>
+     <p id='copyright'>Copyright &copy;<span id='year'>2022</span><br></br>
      <span>+233542352886</span>
      </p>
 
